a12f46b96ff4b401d5bffcb1ab251d9d37654abe
senseyn <novik_alex06@mail.ru>
2025-06-15 22:50:37 +0300

    add to git

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,5 @@
 #=========БИБЛИОТЕКИ СТАНДАРТ========
 import asyncio
-import csv
-import os
 
 from aiogram import Router  # F - магический фильтр
 from aiogram.enums import ChatAction
@@ -11,7 +9,7 @@ from aiogram.types import Message, BotCommand, BotCommandScopeDefault
 # ==========ИМПОРТ МОИХ ФАЙЛОВ=========
 from create_bot import bot
 from handlers.style_text import bot_text_baner
-
+from db.users_csv import add_user_check
 #=====================================
 start_router = Router()
 
@@ -27,12 +25,13 @@ async def set_commands():
 #======================КОМАНДЫ БОТА==============================
 @start_router.message(CommandStart())
 async def cmd_start(message: Message):
+    add_user_check(message.from_user)
     await message.delete()
     await message.bot.send_chat_action(
         chat_id=message.chat.id,
         action=ChatAction.TYPING
     )
-    await asyncio.sleep(0.5)
+    await asyncio.sleep(0.4)
     name = message.from_user.first_name
     text = bot_text_baner(name)  #ОТПРАВЛЯЕМ И ПРИНИМАЕМ ТЕКСТ С ПАРАМЕТРОМ NAME
     await message.answer(text, parse_mode="HTML")
