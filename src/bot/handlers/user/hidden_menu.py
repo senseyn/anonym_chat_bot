b9dc4cc8e5d76975b0ae9585f734586c965406c1
Dev Alexander N. <117079756+senseyn@users.noreply.github.com>
2025-06-18 16:25:08 +0300

    Update hidden_menu.py
    Emoji

--- src/bot/handlers/user/hidden_menu.py
+++ src/bot/handlers/user/hidden_menu.py
@@ -53,5 +53,5 @@ async def hidde_command(message: Message, state: FSMContext):
         await message.delete()
     except Exception as e:
         delete_mess_commands(e)
-    await message.answer('🎲')
+    await message.send_dice(emoji = '🎲')
 
