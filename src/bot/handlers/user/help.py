e951ea30f1bc14438b5abb9e97e598fd1207b040
Dev Alexander N. <117079756+senseyn@users.noreply.github.com>
2025-06-19 16:28:41 +0300

    Update help.py
    import add

--- src/bot/handlers/user/help.py
+++ src/bot/handlers/user/help.py
@@ -1,7 +1,24 @@
+#=========БИБЛИОТЕКИ СТАНДАРТ========
+import asyncio
 
+from aiogram import Router  # - магический фильтр
+from aiogram.enums import ChatAction
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
+from src.bot.states.menu_states import MenuStates
 
+# ==========ИМПОРТ МОИХ ФАЙЛОВ=========
+from src.bot.handlers.user.style_text_user import 
+#======================================
+start_router = Router()
 
+#==========ФУНКЦИИ=====================
+def delete_mess_commands(error):
+    print(f"\033[1;41mОшибка удаления\033[0m: {error}")
+
+#======================КОМАНДЫ БОТА==============================
 @start_router.message(Command('help'), MenuStates.Main)
 async def cmd_start(message: Message, state: FSMContext):
     #========ПЕЧАТАЕТ СТАТУС=========
@@ -17,5 +34,5 @@ async def cmd_start(message: Message, state: FSMContext):
         delete_mess_commands(e)
     # ================================
     name = message.from_user.first_name  # Достаем имя пользователя
-    text = bot_text_baner()
+    text = bot_text_baner(name)
     await message.answer(text, parse_mode="HTML")
