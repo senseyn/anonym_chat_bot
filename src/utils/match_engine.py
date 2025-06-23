f66cd8e79d78f1df7f5f3f76a4ba46ac88f6f698
Dev Alexander N. <117079756+senseyn@users.noreply.github.com>
2025-06-23 17:46:33 +0300

    Update match_engine.py

--- src/utils/match_engine.py
+++ src/utils/match_engine.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 # ==========ИМПОРТ МОИХ ФАЙЛОВ=========
 from src.db.database import Database
 from src.keyboards.user_kb import stop_search_button
-from src.states.menu_states import MenuStates
+from src.states.menu_states import MenuStates, MenuSearch
 
 #======================================
 match_router = Router()
@@ -15,11 +15,11 @@ db = Database(r'src/db/db_handler.db') # база данных
 
 
 @match_router.message(F.text == "❤️ Начать поиск", MenuStates.Main)
-async def hidden_comand_back_kb(message: Message):
+async def hidden_comand_back_kb(message: Message, state: FSMContext):
     chat_id = message.chat.id
     if message.chat.type == "private":
+        await state.set_state(MenuSearch.Search)
         button = await stop_search_button()
         db.add_queue(chat_id) # ДОБАВЛЕНИЕ В БАЗУ
         await message.answer(f"Поиск собеседника", parse_mode="HTML", reply_markup=button)
-
 
