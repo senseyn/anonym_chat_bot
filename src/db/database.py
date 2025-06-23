7c811824f88c93828472da70b2c3242a23f91951
Dev Alexander N. <117079756+senseyn@users.noreply.github.com>
2025-06-23 17:06:31 +0300

    Update database.py

--- src/db/database.py
+++ src/db/database.py
@@ -2,15 +2,12 @@ import sqlite3
 
 
 class Database:
-    # ===========ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ=========
+    #===========ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ=========
     def __init__(self, database_file):
         self.connection = sqlite3.connect(database_file, check_same_thread=False)
         self.cursor = self.connection.cursor()
-
     #===========ДОБАВЛЕНИЕ В ОЧЕРЕДЬ=========
     def add_queue(self, chat_id):
         with self.connection:
-def add_queue(self, chat_id):
-    with self.connection:
-        self.cursor.execute("INSERT INTO queue (chat_id) VALUES (?)", (chat_id,))
-        print("✅ added to queue")
+            self.cursor.execute("INSERT INTO queue (chat_id) VALUES (?)", (chat_id,))
+            print("✅ added to queue")
